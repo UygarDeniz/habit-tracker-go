1dffb1ba19995742d63ae39dfb6008d8553b105c
Uygar Deniz ACAR <uygar.d.acar@gmail.com>
2025-06-12 01:08:31 +0300

    Add trigger to update timestamp on user records

--- migrations/00001_users.sql
+++ migrations/00001_users.sql
@@ -9,9 +9,24 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp_users
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+    EXECUTE FUNCTION trigger_set_timestamp();
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+DROP TRIGGER IF EXISTS set_timestamp_users ON users;
+DROP FUNCTION IF EXISTS trigger_set_timestamp();
 DROP TABLE IF EXISTS users;
 -- +goose StatementEnd
